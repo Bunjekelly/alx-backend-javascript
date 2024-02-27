0fe6ad966fb1b818730dd9d25120e9f0af63e937
Bunje kelly <kellybunje@gmail.com>
2024-02-27 23:48:40 +0100

    smakk server app

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+// a small HTTP server using the http module
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+    res.statusCode = 200; 
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
