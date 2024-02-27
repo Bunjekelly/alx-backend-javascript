6840bcc8af219d1ef9eeff635ce53a2c58778dd1
Bunje Kelly <kellybunje@gmail.com>
2024-02-27 23:57:39 +0000

    fixed error

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,33 +1,32 @@
-// create a small HTTP server using the http module 
+// create a small HTTP server using the http module
 
 const http = require('http');
-const url = require('url');
 const countStudents = require('./3-read_file_async');
 
 const DATABASE_PATH = './database.csv';
 
 const app = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain');
 
-    if (req.url === '/') {
-        res.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        countStudents(DATABASE_PATH)
-            .then((studentData) => {
-                res.write('This is the list of our students\n');
-                res.write(`Number of students: ${studentData.total}\n`);
-                for (const [field, students] of Object.entries(studentData.fields)) {
-                    res.write(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`);
-                }
-                res.end();
-            })
-            .catch((error) => {
-                res.end(error.message);
-            });
-    } else {
-        res.statusCode = 404;
-        res.end('Page not found');
-    }
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(DATABASE_PATH)
+      .then((studentData) => {
+        res.write('This is the list of our students\n');
+        res.write(`Number of students: ${studentData.total}\n`);
+        for (const [field, students] of Object.entries(studentData.fields)) {
+          res.write(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`);
+        }
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Page not found');
+  }
 });
 
 app.listen(1245);
